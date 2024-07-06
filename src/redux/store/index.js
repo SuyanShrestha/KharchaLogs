bcd241bd146e9d7551d4a607cec53b0ab1414429
suyan.shrestha <suyan.shrestha@bajratechnologies.com>
2024-07-06 08:53:04 +0545

    feat: Add initial setup files for project

---
+++ src/redux/store/index.js
@@ -0,0 +1,13 @@
+import {combineReducers, legacy_createStore as createStore} from 'redux';
+import { expenseReducer } from '../reducers/expenses';
+
+// for multiple reducers
+const reducer = combineReducers({
+    expenses: expenseReducer,
+});
+
+const initialState = {};
+
+const store = createStore(reducer, initialState);
+
+export default store;
